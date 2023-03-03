e846671772dce5a8c9d8de8effab5c85be9cba18
Teshome Hailu <teshomehailu9@gmaill.com>
2023-03-03 15:05:01 +0300

    logout component updated

---
+++ src/pages/auth/logout.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <q-btn label="logout" @click="logout()" push color="secondary"
+           text-color="primary" class="float-right"
+    />
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from "stores/auth-store";
+
+export default {
+  name: "logout-component",
+  data() {
+    return {};
+  },
+  methods: {
+    logout: function() {
+      useAuthStore().logout();
+    }
+  }
+};
+</script>
+
+<style>
+
+
+</style>
